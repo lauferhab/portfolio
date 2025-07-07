e5d7364846e198dbaf1db14d891711e3b9092746
lauferhab <lauferhab@gmail.com>
2025-07-07 19:39:26 +0200

    Summary

--- js/script.js
+++ js/script.js
@@ -1,18 +1,9 @@
-function hamburg(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform  = "translateY(0px)"
-}
-function cancel(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform  = "translateY(-500px)"
-}
-
 // for Typewriter effect
 
 const texts = [
-    "laura",
-    "YOUTUBER",
-    "DESIGNER"
+    "Ingeniera Informática",
+    "Desarrolladora Web",
+    "Especialista en Ciberseguridad"
 ]
 
 let speed = 100;
@@ -46,3 +37,36 @@ function eraseText() {
 }
 
 window.onload = typeWriter;
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
